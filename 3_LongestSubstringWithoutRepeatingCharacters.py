69bf0cca0d0c42b341ed8832f59dbdb49b7cfc4b
ChingChieh <garnett.huang@gmail.com>
2020-09-19 14:27:13 -0400

    Sliding window approach

--- 3_LongestSubstringWithoutRepeatingCharacters.py
+++ 3_LongestSubstringWithoutRepeatingCharacters.py
@@ -1,11 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        hashMap = dict()
-        i = 0
+        longest = set()
         result = 0
-        for idx, val in enumerate(s):
-            if val in hashMap:
-                i = max(hashMap[val], i)
-            hashMap[val] = idx + 1
-            result = max(result, idx - i + 1)
+        i, j = 0, 0
+        while(i < len(s) and j < len(s)):
+            if s[j] in longest:
+                result = max(result, len(longest))
+                longest.remove(s[i])
+                i = i + 1
+            else:
+                longest.add(s[j])
+                j = j + 1
+                
+        result = max(result, len(longest))
         return result
+
+'''
+Sliding Window: using i, j as bounds of the longest substing and "longest" as a set() to record 
+                current char in the range [i,j). This approach needs keep deleting char in the set
+                once it found the duplicate and moving i one by one to find the right bound
+'''
